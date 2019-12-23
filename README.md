786c2c30f2dbe5e7e1d42d481fe904e1c0da9234
tommytrg <tomastrg22@gmail.com>
2019-12-23 11:56:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Witnet Marketplace API
+
+Steps to run this project:
+
+1. Run `yarn install` command
+2. Setup database url inside .env file: `MONGODB_URL = ""`
+3. Run `yarn dev` command
---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+   "type": "mongodb",
+   "url": `${process.env.MONGODB_URL}`,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "src/entity/**/*.ts"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
